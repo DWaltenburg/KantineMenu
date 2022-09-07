5fa7056040447708f0dbf8674eb0946643cce164
DWaltenburg <113024423+DWaltenburg@users.noreply.github.com>
2022-09-07 15:18:34 +0200

    Add files via upload

---
+++ KantineMenu/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KantineMenu
+{
+    class Class1
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        public float price { get; set; }
+
+        public string Name { get { return name; } set { name = value; } } 
+        public string Desc { get { return desc; } set { desc = value; } } 
+        public float Price { get { return price; } set { price = value; } } 
+    }
+}
---
+++ KantineMenu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KantineMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.name = "Pizza";
+            class1.price = 28;
+            class1.desc = "Du får et fladt stykke brød af questionable character. " +
+                          "Hvis du forventer spiselige kanter, hold dig væk. " +
+                          "Hvis du elsker at ødelægge din tænder, er du kommet til det rette sted. " +
+                          "Har også salat og to valg af dressing. Luksus.";
+
+            Console.WriteLine($"Dagens menu er {class1.name} for {class1.price} kr");
+            Console.WriteLine(class1.desc);
+          
+        }
+    }
+}
